7870cd38d86eae619b21d23b1d80bc56d26455a5
Bruno Oliveira <brunooliveira812@gmail.com>
2020-02-23 18:39:43 -0300

    adding .net core api for streaming

---
+++ dotnet-api/Controllers/VideoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private readonly ILogger<VideoController> _logger;
+
+        public VideoController(ILogger<VideoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{moviename}")]
+        public IActionResult Get(string moviename)
+        {
+            _logger.LogInformation("Nome do arquivo: {0}", moviename);
+            string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Movies", moviename);
+            
+            _logger.LogInformation("Caminho completo: {0}", fullPath);
+
+            if (System.IO.File.Exists(fullPath)) {
+                _logger.LogInformation("Abrindo arquivo");
+                FileStream fs = System.IO.File.Open(fullPath, FileMode.Open);
+
+                _logger.LogInformation("Iniciando streaming");
+                return this.File(fs, new MediaTypeHeaderValue("video/mp4").MediaType, true);
+            }
+
+            _logger.LogInformation("Bad Request");
+            return BadRequest();
+        }
+    }
+}
---
+++ dotnet-api/Program.cs
@@ -0,0 +1,24 @@
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace dotnet_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(cfg => {
+                        cfg.Listen(IPAddress.Any, 5000);
+                    });
+                });
+    }
+}
